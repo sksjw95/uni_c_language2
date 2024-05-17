4805ec4d59548d3e32603bfb334ceb614775b072
sksjw95 <tjwldnd95@hanmail.net>
2024-05-17 10:57:31 +0900

    1

---
+++ Project1/a.c
@@ -0,0 +1,14 @@
+/*#include<stdio.h>
+void main() {
+	char ch;
+	int in;
+	ch = in = 'A';
+	printf("ch = %d\n", ch);
+	printf("in = %d\n", in);
+	ch = in = 100;
+	printf("\nch = %c\n", ch);
+	printf("in = %c\n", in);
+
+
+
+}*/
