e703884e8ee3aafeac8e5a107986fc6a49b5e719
sksjw95 <tjwldnd95@hanmail.net>
2024-05-18 01:01:26 +0900

    2

---
+++ Project1/workbookex1_1.c
@@ -0,0 +1,16 @@
+/*#include<stdio.h>
+#pragma warning(disable:4996)
+int product(int x, int y);
+void main() {
+	int a, b, c;
+	printf("Enter a number between 1 and 100 : ");
+	scanf("%d", &a);
+	printf("Enter another number between 1 and 100 : ");
+	scanf("%d", &b);
+	c = product(a, b);
+	printf("%d * %d = %d\n", a, b, c);
+}
+
+int product(int x, int y) {
+	return (x * y);
+}*/
