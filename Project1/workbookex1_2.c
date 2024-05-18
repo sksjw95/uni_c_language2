01e3b06adf18f359b59cf950352d1c03e7964b5e
sksjw95 <tjwldnd95@hanmail.net>
2024-05-19 00:01:42 +0900

    3

---
+++ Project1/workbookex1_2.c
@@ -0,0 +1,15 @@
+/*#include <stdio.h>
+#define FZ_PT 32.0
+#define S_FACTOR (5.0/9.0)
+#pragma warning (disable : 4996)
+
+void main() {
+	float fa, ce;
+	printf("Enter Fahrenheit temperature : ");
+	scanf("%f", &fa);
+	 ce = (fa - FZ_PT) * S_FACTOR;
+	 printf("Celsius equivalent : %.1f\n", ce);
+
+
+
+}*/
