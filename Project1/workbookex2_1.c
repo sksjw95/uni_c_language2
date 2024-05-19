11be3737f4b16b4a4f12b988a67a781cc638352f
sksjw95 <tjwldnd95@hanmail.net>
2024-05-20 00:18:32 +0900

    4

---
+++ Project1/workbookex2_1.c
@@ -0,0 +1,19 @@
+/*#include <stdio.h>
+#define PI 3.141592
+const int k = 10;
+
+void main() {
+	double a;
+	a = k + PI;
+	//k = 20;
+	printf("%f", a);
+
+	int x;
+	int y;
+	int z;
+
+	x = 10;
+	y = 20;
+	z = x + y;
+	printf("\n%d", z);
+}*/
