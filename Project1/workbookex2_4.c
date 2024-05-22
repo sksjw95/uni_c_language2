273dd43e60758bb36bbec9e458dc74897536cacc
sksjw95 <tjwldnd95@hanmail.net>
2024-05-23 00:35:01 +0900

    7

---
+++ Project1/workbookex2_4.c
@@ -0,0 +1,11 @@
+/*#include <stdio.h>
+#include <conio.h>
+#include "factorial.h"
+#pragma warning (disable:4996)
+
+void main() {
+	int x;
+	scanf("%d", &x);
+	printf("%d != %d", x, factorial(x));
+	getch();
+}*/
