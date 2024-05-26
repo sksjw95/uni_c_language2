808170afdf47ee68c55fb7bd21293917dc4be77f
sksjw95 <tjwldnd95@hanmail.net>
2024-05-27 08:56:51 +0900

    11

---
+++ Project1/workbookex3_1.c
@@ -0,0 +1,11 @@
+/*#include <stdio.h>
+#pragma warning (disable:4996)
+void main() {
+	int a, b;
+
+	printf("input(integer)==>");
+	scanf("%d", &a);
+	printf("input(integer)==>");
+	scanf("%d", &b);
+	printf("\na= %d, b= %d", a, b);
+}*/
