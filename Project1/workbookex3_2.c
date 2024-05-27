b01ac13330dcf2f47ac62c5f142938fbc5c26a39
sksjw95 <tjwldnd95@hanmail.net>
2024-05-28 01:30:32 +0900

    12

---
+++ Project1/workbookex3_2.c
@@ -0,0 +1,24 @@
+/*#include <stdio.h>
+#pragma warning (disable:4996)
+void main() {
+	int in;
+	float fl;
+	char ch;
+	char str[50];
+
+	printf("문자를 입력한 후 Enter키를 누르세요. \n");
+	scanf("%c", &ch);
+	printf("%c\n", ch);
+
+	printf("정숫값을 입력한 후 Enter키는 누르세요. \n");
+	scanf("%d", &in);
+	printf("%d\n", in);
+
+	printf("실숫값을 입력한 후 Enter키는 누르세요. \n");
+	scanf("%f", &fl);
+	printf("%f\n", fl);
+
+	printf("문자열을 입력한 후 Enter키를 누르세요. \n");
+	scanf("%s", &str);
+	printf("%s\n", str);
+}*/
