4134561e5c718b9c68409d4456b1c57dcab5cb52
sksjw95 <tjwldnd95@hanmail.net>
2024-05-30 00:53:10 +0900

    14

---
+++ Project1/workbookex3_4.c
@@ -0,0 +1,20 @@
+/*#include<stdio.h>
+#pragma warning(disable:4996)
+#define UNIV "한국방송통신대학교"
+#define DEP "컴퓨터과학과"
+void main() {
+	char ch;
+	char name[20];
+
+	printf("이름을 입력하세요.");
+	gets(name);
+	printf("학점을 입력하세요.");
+	ch = getchar();
+	printf("\n\n");
+
+	puts(UNIV);
+	puts(DEP);
+	puts(name);
+	printf("학점은 :");
+	putchar(ch);
+}*/
