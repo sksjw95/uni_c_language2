8a1a9c181fcc66fa10c7ac0e06f9e35979c7f933
sksjw95 <tjwldnd95@hanmail.net>
2024-05-31 00:24:48 +0900

    15

---
+++ Project1/workbookex3_5.c
@@ -0,0 +1,17 @@
+/*#include<stdio.h>
+
+void main() {
+	int a = 10;
+	int b = 20;
+	int c, d;
+	c = a + b;
+	printf("c = %d\n", c);
+	d = a > 5 && b < 10;
+	printf("d = %d\n", d);
+	if (a > b) {
+		printf("a은 b보다 크다.");
+	}
+	else {
+		printf("a은 b보다 작다.");
+	}
+}*/
