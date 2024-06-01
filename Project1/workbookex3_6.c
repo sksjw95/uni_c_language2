fa76964b71d48be1bdaedc5a48cdc488e7678382
sksjw95 <tjwldnd95@hanmail.net>
2024-06-01 12:56:36 +0900

    16

---
+++ Project1/workbookex3_6.c
@@ -0,0 +1,10 @@
+/*#include<stdio.h>
+#pragma warning (disable:4996)
+#define KILO 1.609
+void main() {
+	float miles, kms;
+	printf("\t input(miles) ==>");
+	scanf("%f", &miles);
+	kms = miles * KILO;
+	printf("\t %f miles = %f km \n", miles, kms);
+}*/
