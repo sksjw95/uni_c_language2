6fabfdd9ce196143ae08ee8fa71ffa796d5122b6
sksjw95 <tjwldnd95@hanmail.net>
2024-06-02 00:20:52 +0900

    17

---
+++ Project1/workbookex3_7.c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+void main() {
+	int x = 1;
+
+	printf("x = %d \n", x++); // x = 1 출력, 2으로 증가
+	printf("x = %d \n", x++); // x = 2 출력, 3으로 증가
+	printf("x = %d \n", ++x); // x = 4 출력 4으로 고정
+	printf("x = %d \n", x--); // x = 4 출력, 3으로 감소
+	printf("x = %d \n", x--); // x = 3 출력, 2으로 감소
+	printf("x = %d \n", --x); // x = 1 출력, 1로 고정
+}*/
