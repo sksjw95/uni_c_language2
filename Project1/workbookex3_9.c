68f113008f86a4b2c1a0b3c76ff869552984419f
sksjw95 <tjwldnd95@hanmail.net>
2024-06-04 00:48:38 +0900

    19

---
+++ Project1/workbookex3_9.c
@@ -0,0 +1,10 @@
+/*#include<stdio.h>
+#pragma warning (disable:4996)
+void main() {
+	char ch;
+	int j;
+	printf("문자를 입력하세요. \n");
+	scanf("%c", &ch);
+	j = (ch >= 'a' && ch <= 'z');
+	printf("결과는 %d \n", j);
+}*/
