a64358b6d93508cd1e13aad2f1f501afa33b5c80
sksjw95 <tjwldnd95@hanmail.net>
2024-06-05 09:28:20 +0900

    20

---
+++ Project1/workbookex3_10.c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+#pragma warning (disable:4996)
+void main() {
+	int a, b, c;
+	printf("첫 번째 숫자를 입력하세요 \n");
+	scanf("%d", &a);
+	printf("두 번째 숫자를 입력하세요 \n");
+	scanf("%d", &b);
+	c = (a > b) ? a : b;
+	printf("큰 수는 %d 입니다.", c);
+}*/
