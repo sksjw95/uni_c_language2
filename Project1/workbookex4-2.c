57216dc120b66d9f5536350210d093fbb6221c6c
sksjw95 <tjwldnd95@hanmail.net>
2024-06-09 08:44:08 +0900

    24

---
+++ Project1/workbookex4-2.c
@@ -0,0 +1,23 @@
+/*#include<stdio.h>
+#pragma warning (disable:4996)
+void main() {
+	int score;
+
+	printf("점수를 입력하세요 : ");
+	scanf("%d", &score);
+	if (score >= 90) {
+		printf("%d A \n", score);
+	}
+	else if (score >= 80) {
+		printf("%d B \n", score);
+	}
+	else if (score >= 70) {
+		printf("%d C \n", score);
+	}
+	else if (score >= 60) {
+		printf("%d D \n", score);
+	}
+	else {
+		printf("%d F \n", score);
+	}
+}*/
