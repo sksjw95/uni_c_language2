41549312d5077c10a792da44f8d269f0a9afcb1f
sksjw95 <tjwldnd95@hanmail.net>
2024-06-08 09:50:09 +0900

    23

---
+++ Project1/workbookex4-1.c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+#pragma warning(disable:4996) 
+void main() {
+	int a;
+
+	printf("정수를 입력하시오: ");
+	scanf("%d", &a);
+
+	if (a % 2 == 0) {
+		printf("%d 은(는) 짝수입니다.", a);
+	}
+	if (a % 2 == 1) {
+		printf("%d 은(는) 홀수입니다.", a);
+	}
+}*/
