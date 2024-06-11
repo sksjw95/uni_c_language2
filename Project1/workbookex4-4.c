6e193fb8c66b6c4a52e5b07bbea7bec9d0aa32f0
sksjw95 <tjwldnd95@hanmail.net>
2024-06-11 14:14:34 +0900

    26

---
+++ Project1/workbookex4-4.c
@@ -0,0 +1,16 @@
+/*#include<stdio.h>
+#include<conio.h>
+#pragma warning(disable:4996)
+void main() {
+	int i, j;
+
+	for (i = 7; i < 10; i = i + 3) {
+		for (j = 1; j < 10; j++) {
+			printf("\t %2d * %2d = %2d \t", i, j, i + j);
+			printf("\t %2d * %2d = %2d \t", i, j, (i+1) + j);
+			printf("\t %2d * %2d = %2d \n", i, j, (i+2) + j);
+		}
+		getch();
+		printf("\n");
+	}
+}*/
