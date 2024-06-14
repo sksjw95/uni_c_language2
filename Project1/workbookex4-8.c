f4fe05e4260daf5783964bfa833499c6d6fd760a
sksjw95 <tjwldnd95@hanmail.net>
2024-06-15 00:04:02 +0900

    30

---
+++ Project1/workbookex4-8.c
@@ -0,0 +1,13 @@
+/*#include<stdio.h>
+#pragma warning (disable:4996)
+void main() {
+	int count;
+	int tmp;
+	int total = 0;
+	for (count = 1; count <= 5; count++) {
+		printf("%d번째 정수값을 입력한 후 Enter키를 누르세요. ", count);
+		scanf("%d", &tmp);
+		total += tmp;
+	}
+	printf("읽은 정수의 충합은 %d 입니다. \n", total);
+}*/
