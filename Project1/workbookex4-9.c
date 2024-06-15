626ed560ab2ac0f493a557924a7b3b596783e02d
sksjw95 <tjwldnd95@hanmail.net>
2024-06-16 00:48:11 +0900

    31

---
+++ Project1/workbookex4-9.c
@@ -0,0 +1,13 @@
+/*#include<stdio.h>
+void main() {
+	static char a[31] = "KOREA NATIONAL OPEN UNIVERSITY";
+	int i, c;
+	c = 0;
+	for (i = 0; i <= 29; i++) {
+		if (a[i] == 'O') {
+			c++;
+		}
+
+	}
+	printf(" O 의 개수는 %d개 입니다.", c);
+}*/
