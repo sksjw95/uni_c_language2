57fc8c1cdf1c2a073d8e0fc88a6306b6fd9f0b92
sksjw95 <tjwldnd95@hanmail.net>
2024-06-23 13:48:17 +0900

    37

---
+++ Project1/workbookex5-3.c
@@ -0,0 +1,12 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+#pragma warning (disable:4996)
+void main() {
+	int i, a;
+	printf("input number: ");
+	scanf("%d", &a);
+	srand(a);
+	for (i = 1; i <= 5; i++) {
+		printf("%d \n", rand());
+	}
+}*/
