bd19ba13fb95296ea64dbf159280e5b95288bd74
sksjw95 <tjwldnd95@hanmail.net>
2024-06-24 09:31:54 +0900

    38

---
+++ Project1/workbookex5-4.c
@@ -0,0 +1,17 @@
+/*#include<stdio.h>
+#include<string.h>
+
+void main() {
+	char string[20];
+	printf("문장을 입력한 후, Enter 키를 치세요! \n");
+	printf("아무것도 입력하지 않으면 프로그램은 종료됩니다.!");
+
+	do {
+		gets(string);
+		if (strlen(string) == 0) {
+			break;
+		}
+		printf("문자열의 길이는 %d 입니다.", strlen(string));
+	} while (1);
+
+}*/
