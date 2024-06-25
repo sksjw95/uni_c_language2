88d1d658ceb7f9db082dfe6363e6215c0d9f6cc5
sksjw95 <tjwldnd95@hanmail.net>
2024-06-26 00:39:09 +0900

    40

---
+++ Project1/workbookex5-6.c
@@ -0,0 +1,13 @@
+/*#include<stdio.h>
+#include<ctype.h>
+void main() {
+	char ch;
+	printf("문자나 숫자를 입력하세요\n");
+	ch = getchar();
+	if (isdigit(ch)) {
+		printf("%c is number \n", ch);
+	}
+	else {
+		printf("%c is not number \n", ch);
+	}
+}*/
