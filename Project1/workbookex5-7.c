127938e5585a26a8ae85a47ec437842553a51194
sksjw95 <tjwldnd95@hanmail.net>
2024-06-27 09:28:20 +0900

    41

---
+++ Project1/workbookex5-7.c
@@ -0,0 +1,19 @@
+/*#include<stdio.h>
+#include<ctype.h>
+void main() {
+	printf("문자를 입력하세요. \n");
+	char ch = getchar();
+	if (isupper(ch)) {
+		printf("%c is a uppercase \n", ch);
+		char ch1 = tolower(ch);
+		printf("%c is a lowercase \n", ch1);
+	}
+	else if (islower(ch)) {
+		printf("%c is a lowercase \n", ch);
+		char ch2 = toupper(ch);
+		printf("%c is a uppercase \n", ch2);
+	}
+	else {
+		printf("%c is not alpha character \n", ch);
+	}
+}*/
