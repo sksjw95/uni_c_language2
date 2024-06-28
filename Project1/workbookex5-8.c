47678afd8a8d5c6beb43e2d84feeb51f660a7a2c
sksjw95 <tjwldnd95@hanmail.net>
2024-06-28 09:17:22 +0900

    42

---
+++ Project1/workbookex5-8.c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+#include<string.h>
+#pragma warning(disable:4996)
+void main() {
+	int len;
+	char s[50];
+	strcpy(s, "COMPUTER SCIENCE");
+	printf("%s \n\n", s);
+	len = strlen(s);
+	printf("%d \n\n", len);
+}*/
