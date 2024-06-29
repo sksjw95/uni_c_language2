18b59301a62b3f517845d040eddf2a9956474fd9
sksjw95 <tjwldnd95@hanmail.net>
2024-06-29 13:38:44 +0900

    43

---
+++ Project1/workbookex5-9.c
@@ -0,0 +1,11 @@
+/*#include<stdio.h>
+#include<string.h>
+#pragma warning (disable:4996)
+void main() {
+	char str1[20], str2[20];
+	printf("문자열 두 개를 입력하시오: \n");
+	scanf("%s", &str1);
+	scanf("%s", &str2);
+	strcat(str1, str2);
+	printf("%s \n", str1);
+}*/
