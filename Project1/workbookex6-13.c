21bd279bb58c3678ff9663f6253ea393614609c2
sksjw95 <tjwldnd95@hanmail.net>
2024-07-13 20:40:29 +0900

    57

---
+++ Project1/workbookex6-13.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+void main() {
+	float array[] = { 100.0,200.0,300.0,400.0,500.0 };
+	float* ptr;
+	int len;
+	ptr = &array[0];
+	printf("%.1\n",*ptr);
+	ptr++;
+	printf("%.1\n", *ptr);
+	ptr++;
+	printf("%.1\n", *ptr);
+	ptr++;
+	printf("%.1\n", *ptr);
+	ptr++;
+	printf("%.1\n", *ptr);
+	ptr = array;
+	printf("%.1\n", *(ptr+2));
+	printf("%.1f %.1f\n", (ptr + 0)[0], (array + 0)[0]);
+	printf("%.1f %.1f\n", (ptr + 1)[0], (array + 1)[0]);
+	printf("%.1f %.1f\n", (ptr -1)[2], (array - 1)[2]);
+}*/
