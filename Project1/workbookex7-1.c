5a8bbbc779c9f9f74424e4c777e526f56ff4c38b
sksjw95 <tjwldnd95@hanmail.net>
2024-07-15 10:09:28 +0900

    59

---
+++ Project1/workbookex7-1.c
@@ -0,0 +1,20 @@
+/*#include<stdio.h>
+#include<string.h>
+#pragma warning (disable:4996)
+struct student {
+	char name[30];
+	char phone[20];
+	char address[100];
+};
+
+void main() {
+	struct student info;
+
+	strcpy(info.name, "홍길동");
+	strcpy(info.phone, "02-1234-5678");
+	strcpy(info.address, "서울시 종로구 둥숭동 169");
+
+	printf("이름 : %s \n", info.name);
+	printf("번호 : %s \n", info.phone);
+	printf("주소 : %s \n", info.address);
+}*/
