5dc236a3ead9a219d269149cd0c5562df515a510
sksjw95 <tjwldnd95@hanmail.net>
2024-07-16 09:18:07 +0900

    60

---
+++ Project1/workbookex7-2.c
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+struct member {
+	char* name;
+	char* sex;
+	int age;
+};
+
+void main() {
+	int i;
+	struct member a[4] = { {"Hong, Kil-Dong","male",50},{"Choi, Soon-Hee","female",46},{
+		"Hong, Heon-Jeong","female",3},{"Hong, Yoo-shin","male",2} };
+	for (i = 0; i < 4; i++) {
+		printf("%s %s %d \n", a[i].name, a[i].sex, a[i].age);
+	}
+}*/
