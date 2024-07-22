59e5d34fa8be09235bdb7e0b4afa69d9991cf2d2
sksjw95 <tjwldnd95@hanmail.net>
2024-07-23 00:48:20 +0900

    67

---
+++ Project1/workbookex8-1.c
@@ -0,0 +1,14 @@
+/*#include<stdio.h>
+#pragma warning(disable:4996)
+void main() {
+	FILE* fp;
+	fp = fopen("C:/Users/tjwld/OneDrive/바탕 화면/방통대/1학년1학기/C/sample1", "w");
+	if (fp != NULL) {
+		fputs("대한민국\n", fp);
+		fputs("서울\n", fp);
+		fputs("종로구\n", fp);
+		fputs("동숭동\n", fp);
+		fputs("169번지\n", fp);
+	}
+	fclose(fp);
+}*/
